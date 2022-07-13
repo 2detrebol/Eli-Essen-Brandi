4058ed2c42e57772cc53106c8d3a735f4369b9de
2detrebol <maubrandi@gmail.com>
2022-07-13 10:57:45 -0300

    Agregando BS y Navbar version inicial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './img/logo.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
