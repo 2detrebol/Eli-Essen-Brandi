3bbe61904158342c916e7f282b2d2664183337ce
2detrebol <maubrandi@gmail.com>
2022-07-16 15:55:21 -0300

    para trabajar 2da clase

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -3,24 +3,27 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './NavBar.css';
-import { FaShippingFast, FaShoppingCart } from 'react-icons/fa';
+import { FaShippingFast } from 'react-icons/fa';
+import { BsCart3 } from "react-icons/bs";
+
+
 
 function barraNav() {
+
     return (
         <Navbar fixed="top" className="navContainer">
             <Container className="containerLogoLinks">
                 <div className="navLogo">
-                    <Navbar.Brand href="#home" className="containerImg">
-                        <img className="imgLogo" src="./logo.png" alt="Logo Essen" />
-                    </Navbar.Brand>
+                    <button className="containerDibu"><img className="imglogo" src="./logo.png" alt="Logo Essen" /></button>
                     <div>
                         <FaShippingFast />
                         <p>ENVIOS SIN CARGO A TODO EL PAÍS</p>
                     </div>
-                    <FaShoppingCart className="carritoLogo" />
+                    <i><BsCart3 className="carritoLogo" /><span className="cartMenuNum">0</span></i>
+
                 </div>
                 <div className="containerLinks">
-                    <Nav className="me-auto menuNav">
+                    <Nav className="menuNav">
                         <Nav.Link className="linkNav" href="#cacerolas">Caserolas</Nav.Link>
                         <Nav.Link className="linkNav" href="#sartenes">Sarténes</Nav.Link>
                         <Nav.Link className="linkNav" href="#complementos">Complementos</Nav.Link>
