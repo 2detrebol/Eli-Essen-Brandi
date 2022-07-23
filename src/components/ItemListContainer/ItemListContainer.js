afb5b04848725b60ccff8458cf023ad48cc3f9bf
2detrebol <maubrandi@gmail.com>
2022-07-23 14:22:15 -0300

    agrego 1 card con Item.js

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './ItemListContainer.css'
 import ItemCount from '../ItemCount/ItemCount'
+import Items from './Items'
 
 const ItemListContainer = ({ greeting }) => {
     return (
         <div>
             <h1 className="nombreApp animate__animated animate__bounceIn animate__delay-1s">{greeting}</h1>
             <ItemCount />
+            <Items />
         </div>
     )
 }
