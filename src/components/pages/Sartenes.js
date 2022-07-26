8a63f4309c82ee437e884dc820c462311d1e4c5e
2detrebol <maubrandi@gmail.com>
2022-07-25 22:18:55 -0300

    filtrado de productos por categoria

--- src/components/pages/Sartenes.js
+++ src/components/pages/Sartenes.js
@@ -1,6 +1,24 @@
+import React from 'react'
+import '../ItemListContainer/ItemListContainer.css'
+import { useState, useEffect } from 'react'
+import { getProducts } from '../../asyncMock'
+import ItemList from '../ItemList/ItemList'
+
+
 const Sartenes = () => {
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        getProducts().then(products => {
+            setProducts(products.filter(product => product.category === "Sarten"))
+        })
+    }, [])
+
     return (
-        <h1>Sartenes</h1>
+        <div>
+            <h1 className="nombreApp animate__animated animate__bounceIn animate__delay-1s">Sarténes</h1>
+            <ItemList products={products} />
+        </div>
     )
 }
-export default Sartenes;
+
+export default Sartenes
