617fa5c0a94cb3a626241b08c55cab9245cb4e3f
2detrebol <maubrandi@gmail.com>
2022-07-28 00:28:03 -0300

    para implementar selector de colores en spans

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,13 +1,36 @@
-import React from 'react'
+import React from 'react';
+import "./ItemDetail.css";
 
-const ItemDetail = ({ id, img, name, description, price }) => {
+const ItemDetail = ({ img, name, description, category, price, color }) => {
+    let colores = () => {
+        switch (color) {
+            case "terra":
+                return "#786B60";
+            case "aqua":
+                return "#519692";
+            default: return "#5E1519";
+        }
+    }
+
     return (
-        <div>
-            {id}
-            <h1>{name}</h1>
-            <img src={img} alt={name} />
-            <p>{description}</p>
-            <p>${price}</p>
+        <div className="containerItemDetail">
+            <div className="imgBxItemDetail">
+                <span>{name}</span>
+                <img src={img} alt={name} className="animate__animated animate__fadeInUp animate__delay-3s" />
+                <p className="productDetailColors" >
+                    <span className="aqua" ></span>
+                    <span className="terra active"></span>
+                    <span className="cherry"></span>
+                </p>
+            </div>
+            <div className="detailsItemDetail">
+                <div className="contentItemDetail">
+                    <h2 style={{ color: colores() }}>{category}</h2>
+                    <p>{description}</p>
+                    <h3>$ {price}</h3>
+                    <button style={{ backgroundColor: colores() }}>Comprar</button>
+                </div>
+            </div>
         </div>
     )
 }
