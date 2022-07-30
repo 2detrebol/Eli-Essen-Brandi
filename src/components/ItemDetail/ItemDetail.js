9debcc96a11076495beaa18d8f5ebf05de4ff503
2detrebol <maubrandi@gmail.com>
2022-07-30 14:03:24 -0300

    modifica animacion de precio, todo ok

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -33,8 +33,8 @@ const ItemDetail = ({ img, name, description, category, price, color, stock }) =
                 <div className="contentItemDetail">
                     <h2 className="animate__animated animate__fadeInDown animate__delay-5s" style={{ color: colores() }}>{category}</h2>
                     <p className="animate__animated animate__fadeInUp animate__delay-5s">{description}</p>
-                    <div className="priceContainer animate__animated animate__zoomIn">
-                        <h3 className="animate__animated animate__heartBeat"> $ {price}</h3>
+                    <div className="priceContainer animate__animated animate__flipInX">
+                        <h3 className="animate__animated animate__bounce"> $ {price}</h3>
                     </div>
                     <ItemCount stock={stock} onAdd={handleOnAdd} color={color} />
                 </div>
