6e5e17da45ae5c24cccc414e47d718275bb97ead
2detrebol <maubrandi@gmail.com>
2022-08-04 00:37:53 -0300

    final con cambio de colores segun click en details

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./ItemDetail.css";
 import ItemCount from '../ItemCount/ItemCount';
 
-const ItemDetail = ({ img, name, description, category, price, color, stock }) => {
-    let colores = () => {
-        switch (color) {
+
+const ItemDetail = ({ name, description, category, price, color, stock, imagenes }) => {
+
+    const handleOnAdd = (quantity) => {
+        console.log(`la cantidad agregada es: ${quantity}`);
+    }
+
+    const [cambioColor, setcambioColor] = useState(color);
+    const handleOnClick = (colores) => {
+        setcambioColor(colores);
+    }
+
+    const handleColor = () => {
+        switch (cambioColor) {
             case "terra":
                 return "#786B60";
             case "aqua":
@@ -13,30 +24,26 @@ const ItemDetail = ({ img, name, description, category, price, color, stock }) =
         }
     }
 
-    const handleOnAdd = (quantity) => {
-        console.log(`la cantidad agregada es: ${quantity}`);
-    }
-
 
     return (
         <div className="containerItemDetail">
             <div className="imgBxItemDetail">
                 <span className="animate__animated animate__tada">{name}</span>
-                <img src={img} alt={name} className="animate__animated animate__lightSpeedInLeft animate__delay-1s" />
-                <p className="productDetailColors animate__animated animate__zoomIn animate__delay-2s" >
-                    <span className="aqua" ></span>
-                    <span className="terra active"></span>
-                    <span className="cherry"></span>
-                </p>
+                {cambioColor && <img src={imagenes[cambioColor]} alt={name} className="animate__animated animate__lightSpeedInLeft animate__delay-1s" />}
+                <div className="productDetailColors animate__animated animate__zoomIn animate__delay-2s" >
+                    <button onClick={() => handleOnClick("terra")} className={cambioColor === "terra" ? "active" : ""} style={{ backgroundColor: "#786B60" }}></button>
+                    <button onClick={() => handleOnClick("aqua")} className={cambioColor === "aqua" ? "active" : ""} style={{ backgroundColor: "#519692" }}></button>
+                    <button onClick={() => handleOnClick("cherry")} className={cambioColor === "cherry" ? "active" : ""} style={{ backgroundColor: "#5E1519" }}></button>
+                </div>
             </div>
             <div className="detailsItemDetail">
                 <div className="contentItemDetail">
-                    <h2 className="animate__animated animate__fadeInDown animate__delay-3s" style={{ color: colores() }}>{category}</h2>
+                    <h2 style={{ color: handleColor() }} className="animate__animated animate__fadeInDown animate__delay-3s">{category}</h2>
                     <p className="animate__animated animate__fadeInUp animate__delay-3s">{description}</p>
                     <div className="priceContainer animate__animated animate__flipInX">
-                        <h3 className="animate__animated animate__rubberBand"> $ {price}</h3>
+                        <h3 style={{ color: handleColor() }} className="animate__animated animate__rubberBand"> $ {price}</h3>
                     </div>
-                    <ItemCount stock={stock} onAdd={handleOnAdd} color={color} />
+                    <ItemCount stock={stock} onAdd={handleOnAdd} color={cambioColor} />
                 </div>
             </div>
         </div>
