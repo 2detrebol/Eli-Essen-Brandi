ce8163dbac1bb93856d317084315f5e1999129b1
2detrebol <maubrandi@gmail.com>
2022-09-08 21:09:34 -0300

    final para entrega - todo ok

--- src/components/pages/Recipes.js
+++ src/components/pages/Recipes.js
@@ -21,12 +21,12 @@ const Recipes = () => {
     return (
         <>
             <h1 className="tittleRecipes animate__animated animate__bounceIn">Recetas</h1>
-            <div className="videos">
+            <div className="videos animate__animated animate__fadeIn animate__delay-1s">
                 <video className="video1" width="880" height="560" src="https://i.imgur.com/TQmiDnf.mp4" controls alt="Pepas de membrillo"></video>
                 <video className="video2" width="445" height="275" src="https://i.imgur.com/kejjuHi.mp4" controls alt="Torta húmeda con frutas"></video>
-                <video className="video3" width="445" height="275" src="videofile.ogg" controls alt="Video no soportado"></video>
-                <video className="video4" width="445" height="275" src="videofile.ogg" controls alt="Video no soportado"></video>
-                <video className="video5" width="445" height="275" src="videofile.ogg" controls alt="Video no soportado"></video>
+                <video className="video3" width="445" height="275" src="https://i.imgur.com/2yzqMMb.mp4" controls alt="Brownie"></video>
+                <video className="video4" width="445" height="275" src="https://i.imgur.com/tptFLHT.mp4" controls alt="Cookies con chips"></video>
+                <video className="video5" width="445" height="275" src="https://i.imgur.com/0hQnrAE.mp4" controls alt="Limonies"></video>
 
             </div>
 
